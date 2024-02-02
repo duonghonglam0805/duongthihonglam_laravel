95d7cd7c968b21ce30cc501d509cc9a82c5bf671
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-02-02 09:21:29 +0700

    Laravel bai8: View trong laravel

--- hoclaravel/app/Http/Controllers/Homecontroller.php
+++ hoclaravel/app/Http/Controllers/Homecontroller.php
@@ -8,7 +8,25 @@ class HomeController extends Controller
 {
     // Action index()
     public function index(){
-        return "HOME, Hôm nay học 3 bài nhé Lam";
+        $title = "Học lập trình web tại unicode";
+        $content = "Học laravel 8X tại Unicode nha ok";
+        // $dataView = [
+        //     'titleData' => $title,
+        //     'contentData' => $content,
+        // ];
+        // có thể sử dụng hàm compact thay cho việc khai báo 1 cái mảng 
+        // return view('homepage', compact('title', 'content'));  
+        //Hoặc có thể dùng with
+        // return view('homepage')->with('title',$title)
+        return view('homepage')->with(['title'=>$title, 'content'=>$content]);
+        //Hoặc dùng make
+        // return View::make('homepage',compact('title', 'content') );
+        // return View::make('homepage')->with(['title'=>$title, 'content'=>$content]);
+        // $contentView = view('homepage');
+        // $contentView = $contentView->render();
+        // dd($contentView);
+        // return $contentView;
+        // return "HOME, Hôm nay học 3 bài nhé Lam";
     }
     // Action getNews
     public function getNews(){
@@ -18,5 +36,10 @@ class HomeController extends Controller
     //Action getCategories()
     public function getCategories($id){
         return "Chuyên mục: " . $id;
+    }
+    //
+    public function getProductDetail($id){
+        // return view('clients.products.detail')->with(['id'=>$id]);
+        return view('clients.products.detail', compact('id'));
     }
 }
