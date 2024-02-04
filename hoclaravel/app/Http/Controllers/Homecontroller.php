a12b0aa6d72ccaf1d2c004da8dc2a4611be386a2
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-02-04 11:08:19 +0700

    Laravel Bai11: Blade template engine

--- hoclaravel/app/Http/Controllers/Homecontroller.php
+++ hoclaravel/app/Http/Controllers/Homecontroller.php
@@ -6,40 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // Action index()
+    public $data = [];
     public function index(){
-        $title = "Học lập trình web tại unicode";
-        $content = "Học laravel 8X tại Unicode nha ok";
-        // $dataView = [
-        //     'titleData' => $title,
-        //     'contentData' => $content,
-        // ];
-        // có thể sử dụng hàm compact thay cho việc khai báo 1 cái mảng 
-        // return view('homepage', compact('title', 'content'));  
-        //Hoặc có thể dùng with
-        // return view('homepage')->with('title',$title)
-        return view('homepage')->with(['title'=>$title, 'content'=>$content]);
-        //Hoặc dùng make
-        // return View::make('homepage',compact('title', 'content') );
-        // return View::make('homepage')->with(['title'=>$title, 'content'=>$content]);
-        // $contentView = view('homepage');
-        // $contentView = $contentView->render();
-        // dd($contentView);
-        // return $contentView;
-        // return "HOME, Hôm nay học 3 bài nhé Lam";
+        $this->data['welcome'] = 'Học lập trình laravel tại Unicode';
+        $this->data['content'] = '<h3>Chương 1: Nhập môn laravel:</h3>
+        <p>Kiến thức 1</p>
+        <p>Kiến thức 2</p>
+        <p>Kiến thức 3</p>';
+        $this->data['index'] = 0;
+
+        $this->data['dataArr'] = [
+            'Item 1',
+            'Item 2',
+            'Item 3'
+        ];
+
+        $this->data['number'] = 0;
+        return view('home', $this->data);
     }
-    // Action getNews
-    public function getNews(){
-        $date = date('Y-m-d H:i:s');
-        return "Danh sách tin tức ngày" . $date;
-    }
-    //Action getCategories()
-    public function getCategories($id){
-        return "Chuyên mục: " . $id;
-    }
-    //
-    public function getProductDetail($id){
-        // return view('clients.products.detail')->with(['id'=>$id]);
-        return view('clients.products.detail', compact('id'));
-    }
+  
 }
