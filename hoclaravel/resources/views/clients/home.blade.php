8043bd153c7923b66884e7e1494806d3bf5ad243
Dương Thị Hồng Lam <144437106+duonghonglam0805@users.noreply.github.com>
2024-02-27 09:55:52 +0700

    laravel bai 20: HTTP Response (Tra ve response dang dowload file (#20)

--- hoclaravel/resources/views/clients/home.blade.php
+++ hoclaravel/resources/views/clients/home.blade.php
@@ -21,5 +21,16 @@
     {{-- <x-package-alert/> --}}
     {{-- <x-inputs.button/>
     <x-forms.button/> --}}
-
+        <p><img src="https://cf.shopee.vn/file/e73e521bd3ec1b77277d596fee353d75_tn" alt=""></p>
+        <p><a href="{{route('dowloadImage').'?image=https://cf.shopee.vn/file/e73e521bd3ec1b77277d596fee353d75_tn'}}" class="btn btn-primary">Dowload ảnh</a></p>
+        <p><a href="{{route('dowloadImage').'?image='.public_path('storage/meo.png')}}" class="btn btn-primary">Dowload ảnh</a></p>
+        <p><a href="{{route('dowloadPDF').'?file='.public_path('storage/DuongThiHongLam-Final.pdf')}}" class="btn btn-primary">Dowload tài liệu</a></p>
+    @section('css')
+        <style>
+            img{
+                max-width: 100%;
+                height: auto;
+            }
+        </style>
+    @endsection
 @endsection
