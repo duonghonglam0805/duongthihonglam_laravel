be16a2d765a3c5e4077f4d2a6106f466dbc66492
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-08 16:32:05 +0700

    laravel bai 30: Lam viec voi database phan 3 (update  du lieu)

---
+++ hoclaravel/resources/views/clients/users/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.client')
+{{-- <h1>Dương Thị Hồng Lam</h1> --}}
+@section('title')
+    {{$title}}
+@endsection
+@section('content')
+@if(session('msg'))
+    <div class="alert alert-success">{{session('msg')}}</div>
+@endif
+@if($errors->any())
+    <div class="alert alert-danger">Dữ liệu nhập vào không hợp lệ. Vui lòng kiểm tra lại</div>
+@endif
+    <h1>{{$title}}</h1>
+    <a href="#" class="btn btn-primary">Thêm người dùng</a>
+    <hr >
+    <form action="{{route('users.post-edit')}}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="">Họ và tên</label>
+            <input type="text" name="fullName" class="form-control" id="" placeholder="Họ và tên....." value="{{old('fullName') ?? $userDetail->name}}">
+            @error('fullName')
+                <span style="color: red">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="">Email</label>
+            <input type="text" name="email" class="form-control" id="" placeholder="Email....." value="{{old('email') ?? $userDetail->email}}">
+            @error('email')
+                <span style="color: red">{{$message}}</span>
+            @enderror
+        </div> 
+        <button type="submit" class="btn btn-primary">Cập nhật</button>
+        <a href="{{route('users.index')}}" class="btn btn-warning">Quay lại</a>
+    </form>
+    @endsection
