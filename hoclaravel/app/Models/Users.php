98ce5ff9e43ad72a8724e81ce7ad13a30f4c077e
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-29 18:11:06 +0700

    laravel bai42:Xoa du lieu moi

--- hoclaravel/app/Models/Users.php
+++ hoclaravel/app/Models/Users.php
@@ -20,7 +20,8 @@ class Users extends Model
 
         $users = DB::table($this->table)
             ->select('users.*', 'groups.name as group_name')
-            ->join('groups', 'users.group_id', '=', 'groups.id');
+            ->join('groups', 'users.group_id', '=', 'groups.id')
+            ->where('trash',0);
         $orderBy = 'users.created_at';
         $orderType = 'desc';
         if (!empty($sortByArr) && is_array($sortByArr)) {
@@ -67,7 +68,8 @@ class Users extends Model
 
     public function deleteUser($id)
     {
-        return DB::delete("DELETE FROM $this->table WHERE id= ? ", [$id]);
+        // return DB::delete("DELETE FROM $this->table WHERE id= ? ", [$id]);
+        return DB::table($this->table)->where('id',$id)->delete();
     }
     public function statementUser($sql)
     {
