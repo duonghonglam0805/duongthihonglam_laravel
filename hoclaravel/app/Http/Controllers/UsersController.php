306f6cc082048e3ba26465ef0bd9284970e635ac
Dương Thị Hồng Lam <144437106+duonghonglam0805@users.noreply.github.com>
2024-03-29 17:20:50 +0700

    laravel bai40:Phan trang danh sach (#41)

--- hoclaravel/app/Http/Controllers/UsersController.php
+++ hoclaravel/app/Http/Controllers/UsersController.php
@@ -10,6 +10,7 @@ class UsersController extends Controller
 {
     //
     private $users;
+    const _PER_PAGE = 3;
     public function __construct()
     {
         // $this-> $users= new Users();
@@ -63,7 +64,7 @@ class UsersController extends Controller
             'sortType' =>$sortType
          ];
 
-        $usersList = $this->users->getAllUsers($filters, $keywords, $sortArr);
+        $usersList = $this->users->getAllUsers($filters, $keywords, $sortArr, self::_PER_PAGE);
         return view('clients.users.lists', compact('title', 'usersList', 'sortType'));
     }
     public function add()
--- hoclaravel/resources/views/clients/users/lists.blade.php
+++ hoclaravel/resources/views/clients/users/lists.blade.php
@@ -72,4 +72,5 @@
             @endif
         </tbody>
     </table>
+    {{$usersList->links()}}
     @endsection
