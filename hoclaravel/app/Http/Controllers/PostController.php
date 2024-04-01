8e1529b23e26fd5427058779b21c8394c603590f
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-04-01 22:54:43 +0700

    Bai45: Eloquent ORM - Query trong Eloquent Model

--- hoclaravel/app/Http/Controllers/PostController.php
+++ hoclaravel/app/Http/Controllers/PostController.php
@@ -4,19 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+// use iterable;
 
 class PostController extends Controller
 {
-    public function index(){
-        // return 'ok';                       
+    public function index()
+    {
+        echo '<h2>Query Eloquent Model</h2>';
+        $allPosts = Post::all(); // lấy tất cả bản ghi trong class
+        // // dd($allPosts);
+        // if ($allPosts->count() > 0) //vì két quả trả về collection nên dùng các phương thưc của collection để kiểm tra
+        // {
+        //     foreach ($allPosts as $item) {
+        //         echo $item->title;
+        //         echo "<br>";
+        //     }
+        // }
+        // $detail = Post::find(1);
+        // dd($detail);
+        //Sử dụng query builder
+
+        // $activePosts = DB::table('posts')->where('status', 1)->get();
+        // $activePosts = Post::where('status', 1)
+        // ->orderBy('id','DESC')
+        // ->get();
+
+        // // dd($activePosts);
+        // if ($activePosts->count() > 0)
+        //  //vì két quả trả về collection nên dùng các phương thưc của collection để kiểm tra
+        // {
+        //     foreach ($activePosts as $item) {
+        //         echo $item->title;
+        //         echo "<br>";
+        //     }
+        // }
         // $allPosts = Post::all();
-        // dd($allPosts);
-        // $post = Post::find('c1');
-        // dd($post);
-        $post = new Post();
-        $post->title = 'Bài viết 3';
-        $post->content = 'Nội dung 3';
-        // $post->status = 1;
-        $post->save();
+        // $activePosts=$allPosts->reject(function ($post){ //reject có nghĩa là loại bỏ
+        //     // dd($post);
+        //     return $post->status==0; //boolean
+        // });
+        // dd($activePosts);
+
+        //Muốn xử lý dữ liệu lớn, xử dụng phương thức chunk() để tiết kiệm memory. Phương thức sẽ đưa ra nhỏ hơn dữ liệu và cung cấp qua Closure() để xử lý 
+        // Post::chunk(2, function($posts){
+        //    foreach ($posts as $post){
+        //     echo $post->title.'<br>';
+        //    } 
+        //    echo 'Kết thúc chunk <br>';
+        // });
+            $allPosts = Post::where('status',1)->cursor();
+            foreach($allPosts as $item){
+                echo $item->title.'<br>';
+            }
     }
 }
